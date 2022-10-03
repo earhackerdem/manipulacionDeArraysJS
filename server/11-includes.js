48d241eede3dce1019b16588cb021b79c81048a8
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-10-03 17:33:30 -0500

    reto busqueda en texto case insentive

--- server/11-includes.js
+++ server/11-includes.js
@@ -14,3 +14,17 @@ console.log('for',includeInArray)
 const rta = pets.includes('dog')
 
 console.log('rta',rta)
+
+function solution(word,query){
+    return word.toLowerCase()
+    .includes(query.toLowerCase());
+}
+
+
+const rta1 = solution("Ana lava la tina", "ana");
+const rta2 = solution("Santiago", "tiago");
+const rta3 = solution("Nicolas", "ana");
+
+console.log('rta1',rta1)
+console.log('rta2',rta2)
+console.log('rta3',rta3)
