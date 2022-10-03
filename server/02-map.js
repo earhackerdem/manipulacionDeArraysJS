d5f81616e41d679719f20a2394231bcc7489ce7a
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-10-03 12:02:16 -0500

    Qué es el método map

---
+++ server/02-map.js
@@ -0,0 +1,11 @@
+const letters =  ['a','b','c'];
+
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + '++');
+// }
+
+const newArray = letters.map( letter => letter +'++');
+
+console.log('original',letters)
+console.log('new',newArray)
