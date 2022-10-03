4eb3979c34f5e8abdb0f80a89007b68434f16a58
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-10-03 16:14:49 -0500

    Some

---
+++ server/07-some.js
@@ -0,0 +1,90 @@
+const numbers = [1, 2, 3, 4];
+
+let rta = false;
+
+for (let index = 0; index < numbers.length; index++) {
+    const element = numbers[index];
+    if (element % 2 === 0) {
+        rta = true;
+        break;
+    }
+}
+
+console.log(rta)
+
+
+const rta2 = numbers.some(item => item % 2 === 0)
+console.log('rta2', rta2)
+
+
+const orders = [
+    {
+        customerName: "Nicolas",
+        total: 60,
+        delivered: true,
+    }, {
+        customerName: "Nicolas",
+        total: 6023,
+        delivered: false,
+    },
+    {
+        customerName: "Zulema",
+        total: 120,
+        delivered: false,
+    },
+    {
+        customerName: "Santiago",
+        total: 180,
+        delivered: true,
+    },
+    {
+        customerName: "Valentina",
+        total: 240,
+        delivered: true,
+    }
+];
+
+const rta3 = orders.some(order => order.delivered);
+console.log('rta3', rta3)
+
+
+const dates = [
+    {
+        startDate: new Date(2021, 1, 1, 10),
+        endDate: new Date(2021, 1, 1, 11),
+        title: "Cita de trabajo",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita con mi jefe",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 20),
+        endDate: new Date(2021, 1, 1, 21),
+        title: "Cena",
+    },
+];
+
+const newAppointment = {
+    startDate: new Date(2021, 1, 1, 8),
+    endDate: new Date(2021, 1, 1, 9, 30),
+};
+
+var areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping')
+
+const isOverlap = (newDate) => {
+    return dates.some(date => {
+        return areIntervalsOverlapping(
+            { start: date.startDate, end: date.endDate },
+            { start: newDate.startDate, end: newDate.endDate }
+        )
+    })
+}
+
+console.log('isOverlap',isOverlap(newAppointment))
+
+const datesOverlap = (start1, end1, start2, end2) => {
+    if (start1 <= end2 && start2 <= end1) return true
+    else return false;
+}
