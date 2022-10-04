d89ddcda061838eb6df6a629b5128630a9bd4a37
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-10-04 16:14:22 -0500

    reto flatmap

---
+++ server/15-flat.js
@@ -0,0 +1,56 @@
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"] },
+    { userId: 2, username: "Mike", attributes: ["Lovely"] },
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"] },
+]
+
+const rta = users.map(user => user.attributes).flat()
+console.log('map-flat', rta)
+const rta2 = users.flatMap(user => user.attributes);
+console.log('flat-map', rta2)
+
+const calendars = {
+    primaryCalendar: [
+        {
+            startDate: new Date(2021, 1, 1, 15),
+            endDate: new Date(2021, 1, 1, 15, 30),
+            title: "Cita 1",
+        },
+        {
+            startDate: new Date(2021, 1, 1, 17),
+            endDate: new Date(2021, 1, 1, 18),
+            title: "Cita 2",
+        },
+    ],
+    secondaryCalendar: [
+        {
+            startDate: new Date(2021, 1, 1, 12),
+            endDate: new Date(2021, 1, 1, 12, 30),
+            title: "Cita 2",
+        },
+        {
+            startDate: new Date(2021, 1, 1, 9),
+            endDate: new Date(2021, 1, 1, 10),
+            title: "Cita 4",
+        },
+    ],
+};
+
+const rta3 = Object.values(calendars).flatMap(item => {
+    return item.map(date => date.startDate)
+})
+console.log(rta3)
+console.log('object values', Object.values(calendars).map(item => item.map(date => date.startDate)).flat())
+
+function solution(lines) {
+	return lines.flatMap( element => element.split(' ')).length;
+}; 
+
+rta = solution([
+    "Beautiful is better than ugly",
+    "Explicit is better than implicit",
+    "Simple is better than complex",
+    "Complex is better than complicated",
+  ]);
+
+  console.log(rta)
